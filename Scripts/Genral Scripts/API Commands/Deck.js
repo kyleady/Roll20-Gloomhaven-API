255fb668001951f5858bd5e065bc4acd3001fded
Kyle Smith <kyledavidady@gmail.com>
2020-05-01 22:19:25 -0700

    Modify recallDeckFn, shuffleDeckFn, and playDeckFn to accept paramters to be overwritten. Return card obj after it is played with playDeckFn. Add flipCardFn and !card flip API.

--- Scripts/Genral Scripts/API Commands/Deck.js
+++ Scripts/Genral Scripts/API Commands/Deck.js
@@ -68,26 +68,53 @@ function rebuildDeckFn(matches, msg) {
   whisper(deckNotification(deck, {"cards": msg.selected.length-1 , "status": "Rebuilt"}), {speakingTo: msg.playerid, gmEcho: true})
 }
 
-function recallDeckFn(matches, msg) {
-  var suggestion = '!deck recall $'
-  var deckPhrase = matches[1] || ''
-  var decks = suggestCMD(suggestion, deckPhrase, msg.playerid, 'deck', obj => playerIsGM(msg.playerid) || obj.get('showplayers'))
-  if(!decks) return;
-  var deck = decks[0]
+function recallDeckFn(matches, msg, options) {
+  options = options || {}
+  if(options.alert == undefined) options.alert = true
+  let deck = undefined
+  if(!options.deckid) {
+    let suggestion = '!deck recall $'
+    let deckPhrase = matches[1] || ''
+    let decks = suggestCMD(suggestion, deckPhrase, msg.playerid, 'deck', obj => playerIsGM(msg.playerid) || obj.get('showplayers'))
+    if(!decks) return;
+    deck = decks[0]
+  } else {
+    deck = getObj('deck', options.deckid)
+  }
+
+  const discardCardids = deck.get("_discardPile").split(",").map(cardid => `cardid:${cardid}`)
+  log(discardCardids)
+  const discardCardsAsGraphics = filterObjs(obj => {
+    if(obj.get('_type') != 'graphic') return false;
+    return discardCardids.includes(obj.get('gmnotes'))
+  })
+
+  _.each(discardCardsAsGraphics, discardCardAsGraphic => discardCardAsGraphic.remove())
   recallCards(deck.id, "graphic")
-  whisper(deckNotification(deck, {"status": "Recalled"}), {speakingTo: msg.playerid, gmEcho: true})
+
+  if(options.alert) {
+    whisper(deckNotification(deck, {"status": "Recalled"}), {speakingTo: msg.playerid, gmEcho: true})
+  }
 }
 
-function shuffleDeckFn(matches, msg) {
-  var suggestion = '!deck shuffle $'
-  var deckPhrase = matches[1] || ''
-  var decks = suggestCMD(suggestion, deckPhrase, msg.playerid, 'deck', obj => playerIsGM(msg.playerid) || obj.get('showplayers'))
-  if(!decks) return;
-  var deck = decks[0]
-  if(shuffleDeck(deck.id)) {
-    whisper(deckNotification(deck, {"status": "Shuffled"}), {speakingTo: msg.playerid, gmEcho: true})
+function shuffleDeckFn(matches, msg, options) {
+  options = options || {}
+  let deck = undefined
+  if(!options.deckid) {
+    var suggestion = '!deck shuffle $'
+    var deckPhrase = matches[1] || ''
+    var decks = suggestCMD(suggestion, deckPhrase, msg.playerid, 'deck', obj => playerIsGM(msg.playerid) || obj.get('showplayers'))
+    if(!decks) return;
+    deck = decks[0]
   } else {
-    whisper(deck.get('name') + ' failed to shuffle.', {speakingTo: msg.playerid})
+    deck = getObj('deck', options.deckid)
+  }
+
+  recallDeckFn([], msg, { deckid: deck.id, alert: false })
+  if(shuffleDeck(deck.id)) {
+    whisper(deckNotification(deck, { status: "Shuffled" }), { speakingTo: msg.playerid, gmEcho: true })
+  } else {
+    whisper(deck.get('name') + ' failed to shuffle.', { speakingTo: msg.playerid })
   }
 }
 
@@ -177,29 +204,42 @@ function removeDeckFn(matches, msg) {
   })
 }
 
-function playDeckFn(matches, msg) {
-  var suggestion = '!deck play $'
-  var deckPhrase = matches[1] || ''
-  var decks = suggestCMD(suggestion, deckPhrase, msg.playerid, 'deck', obj => playerIsGM(msg.playerid) || obj.get('showplayers'))
-  if(!decks) return;
-  var deck = decks[0]
-  var pageid = getPlayerPageID(msg.playerid)
-  const playZones = findObjs({
-    _type: 'graphic',
-    _pageid: pageid,
-    layer: 'gmlayer',
-    name: `${deck.get('name')} INK CardPlayer`
-  })
-
-  if(playZones == 0) {
-    whisper(`WARNING: The Deck ${deck.get('name')} does not have a designated play zone. Please contact your GM.`, {speakingTo: msg.playerid})
-    return
-  } else if(playZones > 1) {
-    whisper(`WARNING: There is more than one play zone for the Deck ${deck.get('name')}. Plese contact your GM.`, {speakingTo: msg.playerid})
-    return
+function playDeckFn(matches, msg, options) {
+  options = options || {}
+  let deck = undefined
+  let playZone = undefined
+  if(!options.deckid) {
+    var suggestion = '!deck play $'
+    var deckPhrase = matches[1] || ''
+    var decks = suggestCMD(suggestion, deckPhrase, msg.playerid, 'deck', obj => playerIsGM(msg.playerid) || obj.get('showplayers'))
+    if(!decks) return;
+    deck = decks[0]
+  } else {
+    deck = getObj('deck', options.deckid)
   }
 
-  const playZone = playZones[0]
+  if(!options.playZoneId) {
+    var pageid = getPlayerPageID(msg.playerid)
+    const playZones = findObjs({
+      _type: 'graphic',
+      _pageid: pageid,
+      layer: 'gmlayer',
+      name: `${deck.get('name')} INK CardPlayer`
+    })
+
+    if(playZones == 0) {
+      whisper(`WARNING: The Deck ${deck.get('name')} does not have a designated play zone. Please contact your GM.`, {speakingTo: msg.playerid})
+      return
+    } else if(playZones > 1) {
+      whisper(`WARNING: There is more than one play zone for the Deck ${deck.get('name')}. Plese contact your GM.`, {speakingTo: msg.playerid})
+      return
+    }
+
+    playZone = playZones[0]
+  } else {
+    playZone = getObj('graphic', options.playZoneId)
+  }
+
   const cardid = drawCard(deck.id)
   if(!cardid) {
     whisper(`WARNING: Deck ${deck.get('name')} has no more cards to play.`)
@@ -209,6 +249,7 @@ function playDeckFn(matches, msg) {
   fixedPlayCardToTable(cardid, { left: playZone.get('left'), top: playZone.get('top'), pageid: getPlayerPageID(msg.playerid)} )
   const card = getObj('card', cardid)
   announce(deckNotification(deck, {"card": card, "status": "Played"}))
+  return card
 }
 
 function dumpDeckFn(matches, msg) {
@@ -242,12 +283,6 @@ function dumpDeckFn(matches, msg) {
   whisper(deckNotification(deck, {"dumped": cardsToDump}), {speakingTo: msg.playerid, gmEcho: true})
 }
 
-/*
-ToDo:
-  Top: Calcuate Final Order -> Recall selected -> Shuffle with Order (not discard)
-  Bottom: ^
-*/
-
 on('ready',function(){
   CentralInput.addCMD(/^!\s*deck\s+show\s+(\S.*)$/i, showDeckFn, true);
   CentralInput.addCMD(/^!\s*deck\s+hide\s+(\S.*)$/i, hideDeckFn, true);
@@ -259,16 +294,4 @@ on('ready',function(){
   CentralInput.addCMD(/^!\s*deck\s+remove\s+(\S.*)$/i, removeDeckFn,true);
   CentralInput.addCMD(/^!\s*deck\s+play\s+(\S.*)$/i, playDeckFn,true);
   CentralInput.addCMD(/^!\s*deck\s+dump\s+(\S.*)$/i, dumpDeckFn,true);
-  /*
-  CentralInput.addCMD(/^!\s*deck\s+top\s+(\S.*)$/i, ?,true);
-  CentralInput.addCMD(/^!\s*deck\s+bottom\s+(\S.*)$/i, ?,true);
-  !deck take [deck name]
-  !deck takeall [deck name]
-  !
-  */
-
-  /*
-  !card flip (card or multisided -> next side)
-  !card play [card name] | [deck name]
-  */
 });
